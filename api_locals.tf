b05f6c54dde57ecc49f33978529faa5849f02a68
patrickmdey <patrickmdey@hotmail.com>
2022-07-17 17:50:21 -0300

    category lambda zips

--- api_locals.tf
+++ api_locals.tf
@@ -7,6 +7,16 @@ locals {
         path   = "users"
       }
 
+      categories = {
+        parent = ""
+        path  = "categories"
+      }
+
+      category = {
+        parent = "categories"
+        path   = "{id}"
+      }
+
       timesheets = {
         parent = ""
         path   = "timesheets"
@@ -71,6 +81,48 @@ locals {
         path        = "timesheets"
         lambda_name = "post_timesheet"
       }
+
+      delete_timesheet = {
+        method      = "DELETE"
+        full_path   = "timesheets/{id}"
+        path        = "{id}"
+        lambda_name = "delete_timesheet"
+      }
+
+      get_category = {
+        method      = "GET"
+        full_path   = "categories/{id}"
+        path        = "{id}"
+        lambda_name = "get_category"
+      }
+
+      put_category = {
+        method      = "PUT"
+        full_path   = "categories/{id}"
+        path        = "{id}"
+        lambda_name = "put_category"
+      }
+
+      post_category = {
+        method      = "POST"
+        full_path   = "categories"
+        path        = "categories"
+        lambda_name = "post_category"
+      }
+
+      delete_category = {
+        method      = "DELETE"
+        full_path   = "categories/{id}"
+        path        = "{id}"
+        lambda_name = "delete_category"
+      }
+
+      get_categories = {
+        method      = "GET"
+        full_path   = "categories"
+        path        = "categories"
+        lambda_name = "get_categories"
+      }
     }
   }
 
@@ -88,10 +140,10 @@ locals {
         handler = "put_user.handler"
         runtime = "nodejs16.x"
       }
-      create_user = {
-        name    = "create-user"
-        zip     = "./resources/lambdas/create_user.zip"
-        handler = "create_user.handler"
+      post_user = {
+        name    = "post-user"
+        zip     = "./resources/lambdas/post_user.zip"
+        handler = "post_user.handler"
         runtime = "nodejs16.x"
       }
       get_report = {
@@ -116,6 +168,42 @@ locals {
         name    = "post-timesheet"
         zip     = "./resources/lambdas/post_timesheet.zip"
         handler = "post_timesheet.handler"
+        runtime = "nodejs16.x"
+      }
+      delete_timesheet = {
+        name    = "delete-timesheet"
+        zip     = "./resources/lambdas/delete_timesheet.zip"
+        handler = "delete_timesheet.handler"
+        runtime = "nodejs16.x"
+      }
+      get_category = {
+        name    = "get-category"
+        zip     = "./resources/lambdas/get_category.zip"
+        handler = "get_category.handler"
+        runtime = "nodejs16.x"
+      }
+      put_category = {
+        name    = "put-category"
+        zip     = "./resources/lambdas/put_category.zip"
+        handler = "put_category.handler"
+        runtime = "nodejs16.x"
+      }
+      post_category = {
+        name    = "post-category"
+        zip     = "./resources/lambdas/post_category.zip"
+        handler = "post_category.handler"
+        runtime = "nodejs16.x"
+      }
+      delete_category = {
+        name    = "delete-category"
+        zip     = "./resources/lambdas/delete_category.zip"
+        handler = "delete_category.handler"
+        runtime = "nodejs16.x"
+      }
+      get_categories = {
+        name    = "get-categories"
+        zip     = "./resources/lambdas/get_categories.zip"
+        handler = "get_categories.handler"
         runtime = "nodejs16.x"
       }
     }
